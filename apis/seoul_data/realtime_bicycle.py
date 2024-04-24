47ff310383a89d0220500aa4a31b4a9c3b277eaa
hjkim <hjkim_sun@naver.com>
2024-04-24 21:48:57 +0900

    ch3.7 fix

--- apis/seoul_data/realtime_bicycle.py
+++
@@ -1,9 +0,0 @@
-import requests
-class RealtimeBicycle():
-
-    def __init__(self):
-        pass
-
-
-    def call_api(self):
-        auth_key = '##auth_key_seoul_data##'
--- config/after_install.sh
+++
@@ -1,1 +0,0 @@
-python3 /src/kafka-producer/config/deploy.py
