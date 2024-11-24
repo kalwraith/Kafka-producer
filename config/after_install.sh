2e430ba4fe511e1b2448d489969ab75bdbcde6b3
hjkim <hjkim_sun@naver.com>
2024-11-24 15:36:40 +0900

    source 명령 추가

--- config/after_install.sh
+++ config/after_install.sh
@@ -1,2 +1,3 @@
+source /src/kafka_venv/bin/activate
 pip3 install -r /src/kafka-producer/requirements.txt > /src/kafka-producer/codedeploy.log
 python3 /src/kafka-producer/config/deploy.py > /src/kafka-producer/codedeploy.log
