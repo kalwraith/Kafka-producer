6c9c73fbe10284593d601129c6a4bfcc0c97bd19
hjkim <hjkim_sun@naver.com>
2024-11-24 15:40:44 +0900

    로그 제거

--- config/after_install.sh
+++ config/after_install.sh
@@ -1,3 +1,3 @@
 source /src/kafka_venv/bin/activate
-pip3 install -r /src/kafka-producer/requirements.txt > /src/kafka-producer/codedeploy.log
-python3 /src/kafka-producer/config/deploy.py > /src/kafka-producer/codedeploy.log
+pip3 install -r /src/kafka-producer/requirements.txt
+python3 /src/kafka-producer/config/deploy.py
