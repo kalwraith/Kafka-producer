ecf6fa908fcd3e5890686429bfa4b1ee8f88eb96
hjkim <hjkim_sun@naver.com>
2024-11-24 15:31:24 +0900

    로그 기능 다시

--- config/after_install.sh
+++ config/after_install.sh
@@ -1,2 +1,2 @@
-pip3 install -r /src/kafka-producer/requirements.txt
-python3 /src/kafka-producer/config/deploy.py
+pip3 install -r /src/kafka-producer/requirements.txt > /src/kafka-producer/codedeploy.log
+python3 /src/kafka-producer/config/deploy.py > /src/kafka-producer/codedeploy.log
