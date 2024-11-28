781b34620628ab8bb4add506f4f5b08e59e249d8
hyunjinkim <kalwraith@naver.com>
2024-11-28 15:05:17 +0900

    ch5.9 작성

--- producers/bicycle_producer.py
+++ producers/bicycle_producer.py
@@ -14,7 +14,10 @@ class BicycleProducer():
     def __init__(self, topic):
         self.topic = topic
         self.conf = {'bootstrap.servers': BROKER_LST,
-                     'compression.type':'lz4'}
+                     'compression.type':'lz4',
+                     'enable.idempotence':'true',
+                     'max.in.flight.requests.per.connection':'5',
+                     'acks':'all'}
         self.producer = Producer(**self.conf)
         self._set_logger()
 
