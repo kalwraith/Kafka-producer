991393d16ebe23ef9e9ed83b97de3a1b284ab7f3
hyunjinkim <kalwraith@naver.com>
2024-12-10 11:49:43 +0900

    after_install로 이동

--- config/after_install.sh
+++ config/after_install.sh
@@ -1,3 +1,9 @@
 source /src/kafka_venv/bin/activate
 python3 /src/kafka-producer/config/deploy.py
 pip3 install -r /src/kafka-producer/requirements.txt
+
+# kafka01 서버 bicycle-producer restart
+if [ "$HOSTNAME" == "kafka01" ]; then
+  systemctl stop bicycle-producer.service
+  systemctl start bicycle-producer.service
+fi
